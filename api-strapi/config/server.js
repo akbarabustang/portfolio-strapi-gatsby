56b342ebc4cee96119bbc9dc9aea6b24a9c088e7
Akbar Abustang <akbar@tabsquare.com>
2021-05-14 23:37:11 +0700

    api and frontend structures

---
+++ api-strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ce83d4475958b7af2540f27f4c46c229'),
+    },
+  },
+});
---
+++ api-strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ca95628e-abc4-4ccc-b18a-18bee1fba31b'
+};
